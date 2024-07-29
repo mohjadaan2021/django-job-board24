49683dac33ed672bedeabafbe2bd580cba2ef51c
mohjadaan <unlamarjad@gmail.com>
2024-07-29 11:19:58 +0300

    justify import library

--- Job/urls.py
+++ Job/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import include,path
 from .import views
+
 urlpatterns = [
     path('', views.job_list),
     path('<int:id>', views.job_detail), # link page using id to view def (id)
